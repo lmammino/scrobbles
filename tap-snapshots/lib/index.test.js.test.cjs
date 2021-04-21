388698bf52181a3fce02be7ec84837740edbf1c0
Luciano <lucianomammino@gmail.com>
2021-04-21 17:57:39 +0100

    Added first real test

---
+++ tap-snapshots/lib/index.test.js.test.cjs
@@ -0,0 +1,88 @@
+/* IMPORTANT
+ * This snapshot file is auto-generated, but designed for humans.
+ * It should be checked into source control and tracked carefully.
+ * Re-generate by setting TAP_SNAPSHOT=1 and running tests.
+ * Make sure to inspect the output below.  Do not ignore changes!
+ */
+'use strict'
+exports['lib/index.test.js TAP it traverses multiple pages and collects all songs > songs 1'] = `
+Array [
+  Object {
+    "album": "Build A Tower",
+    "artist": "The Slow Readers Club",
+    "date": 1619019224,
+    "name": "Lunatic",
+    "url": "https://www.last.fm/music/The+Slow+Readers+Club/_/Lunatic",
+  },
+  Object {
+    "album": "Days of Thunder",
+    "artist": "The Midnight",
+    "date": 1619018900,
+    "name": "Days of Thunder",
+    "url": "https://www.last.fm/music/The+Midnight/_/Days+of+Thunder",
+  },
+  Object {
+    "album": "Horror Show",
+    "artist": "The Midnight",
+    "date": 1618998647,
+    "name": "Neon Medusa",
+    "url": "https://www.last.fm/music/The+Midnight/_/Neon+Medusa",
+  },
+  Object {
+    "album": "The Dream",
+    "artist": "Sanctus Real",
+    "date": 1618998318,
+    "name": "The Dream",
+    "url": "https://www.last.fm/music/Sanctus+Real/_/The+Dream",
+  },
+  Object {
+    "album": "The Music That Died Alone",
+    "artist": "The Tangent",
+    "date": 1618998074,
+    "name": "In Dark Dreams",
+    "url": "https://www.last.fm/music/The+Tangent/_/In+Dark+Dreams",
+  },
+  Object {
+    "album": "The Music That Died Alone",
+    "artist": "The Tangent",
+    "date": 1618997643,
+    "name": "Up-hill From Here",
+    "url": "https://www.last.fm/music/The+Tangent/_/Up-hill+From+Here",
+  },
+  Object {
+    "album": "Light Grenades",
+    "artist": "Incubus",
+    "date": 1618997416,
+    "name": "Anna Molly",
+    "url": "https://www.last.fm/music/Incubus/_/Anna+Molly",
+  },
+  Object {
+    "album": "Extended Play",
+    "artist": "Spector",
+    "date": 1618997200,
+    "name": "When Did We Get So Normal?",
+    "url": "https://www.last.fm/music/Spector/_/When+Did+We+Get+So+Normal%3F",
+  },
+  Object {
+    "album": "Build A Tower",
+    "artist": "The Slow Readers Club",
+    "date": 1618996987,
+    "name": "Lunatic",
+    "url": "https://www.last.fm/music/The+Slow+Readers+Club/_/Lunatic",
+  },
+  Object {
+    "album": "Days of Thunder",
+    "artist": "The Midnight",
+    "date": 1618996663,
+    "name": "Days of Thunder",
+    "url": "https://www.last.fm/music/The+Midnight/_/Days+of+Thunder",
+  },
+  Object {
+    "album": "Horror Show",
+    "artist": "The Midnight",
+    "date": 1618996359,
+    "name": "Neon Medusa",
+    "url": "https://www.last.fm/music/The+Midnight/_/Neon+Medusa",
+  },
+]
+`
